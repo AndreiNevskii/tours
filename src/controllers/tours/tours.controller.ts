94c327f090790295882f643e5e37f43eb3f9c7e7
gloc54 <andreinevskiispb@gmail.com>
2025-05-16 18:50:49 +0300

    Tours - remove/add operations

--- src/controllers/tours/tours.controller.ts
+++ src/controllers/tours/tours.controller.ts
@@ -1,17 +1,118 @@
-import { Controller, Delete, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ITour } from 'src/interfaces/tour';
+import { Tour } from 'src/schemas/tour';
 import { ToursService } from 'src/services/tours/tours.service';
 
 @Controller('tours')
 export class ToursController {
     constructor (private toursService: ToursService) {}
 
-     @Post()
-    initTours(): void {
+//    @Post()
+//     initTours(): void {
+//         this.toursService.generateTours();
+//     }
+   
+//      @Delete()
+//     removeAllTours(): void {
+//        this.toursService.deleteTours();
+//     }
+   
+ @Post()
+    initTours(): Promise<ITour[]> {
         this.toursService.generateTours();
+        return this.toursService.getAllTours();
     }
 
     @Delete()
     removeAllTours(): void {
        this.toursService.deleteTours();
     }
+
+    @Get()
+        getAllTours(): Promise<Tour[]> {
+            return this.toursService.getAllTours();
+        }
+
+    // @Get(":id")
+    //     getTourById(@Param('id') id): Promise<Tour> {
+    //         return this.toursService.getTourById(id);
+    //     }
+
+//     @Get(":id")
+//          getTourById(@Param('id', ValidationParamIdPipe) id): Promise<Tour|null> {
+//              return this.toursService.getTourById(id);
+//          }
+// }  
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+    //  @Get()
+    //  getAllTours(): void {
+    //  this.toursService.generateTours();
+    // }
+
+    // @Get(":remove")
+    // removeAllTours(@Param('remove') remove): void {
+    //    this.toursService.deleteTours();
+    // }
+
+
+
+
+
+
+
+
+
+    //   @Post()
+    //   initTours(): void {
+    //  this.toursService.generateTours();
+    //   }
+
+    // @Delete(":remove")
+    // removeAllTours(@Param('remove') remove): void {
+    //    this.toursService.deleteTours();
+    // }
+
+    //   @Post()
+    //  initTours(): Promise<ITour[]> {
+    //      this.toursService.generateTours();
+    //      return this.toursService.getAllTours();
+    //  }
+
+    //  @Delete()
+    //  removeAllTours(): void {
+    //     this.toursService.deleteTours();
+    //  }
+
+    //  @Get()
+    //  getAllTours(): Promise<Tour[]> {
+    //          return this.toursService.getAllTours();
+    //     }
+
+    // @Get(":id")
+    //     getTourById(@Param('id') id): Promise<Tour> {
+    //         return this.toursService.getTourById(id);
+    //     }
+
+    // @Get(":id")
+    //      getTourById(@Param('id', ValidationParamIdPipe) id): Promise<Tour|null> {
+    //          return this.toursService.getTourById(id);
+    //      }
 }
--- src/services/tours/tours.service.ts
+++ src/services/tours/tours.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TourDto } from 'src/dto/tour-dto';
+import { ITour } from 'src/interfaces/tour';
 import { Tour, TourDocument } from 'src/schemas/tour';
 
 @Injectable()
@@ -11,16 +12,26 @@ export class ToursService {
 
     constructor(@InjectModel(Tour.name) private tourModel: Model<TourDocument>) {}
 
-    generateTours(): void {
+   async generateTours(): Promise<any>{
   for (let i = 0; i <= this.toursCount; i++) {
     const tour = new TourDto('test'+i, 'test description', 'test operator', '300'+i);
     const tourData = new this.tourModel(tour);
-    tourData.save();
+    await tourData.save();
    }
   }
 
   async deleteTours(): Promise<any> {
            return this.tourModel.deleteMany({});
          }
+
+
+   async getAllTours(): Promise<ITour[]> {
+          return this.tourModel.find();
+      }
+
+
+  async getTourById(id): Promise<ITour|null> {
+        return this.tourModel.findById(id);
+    }              
      
 }
